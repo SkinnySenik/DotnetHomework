95ba8a2ac9069bebbb9068a2957727b91b49b5c9
Arseny <senik006kup@gmail.com>
2022-11-18 17:12:47 +0300

    Dotnet first homework

---
+++ HomeworkDotnet1/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 2.
+  // Console.Write("Type the first number ");
+  // int a = int.Parse(Console.ReadLine()!);
+  // Console.Write("Type the second number ");
+  // int b = int.Parse(Console.ReadLine()!);
+
+  // if (a > b) {
+  //  int max = a;
+  //  Console.WriteLine($"number a is greater than b. number a = {max}");
+  //} else if (b > a) {
+  //  int max = b;
+  //  Console.WriteLine($"number b is greater than a. number b = {max}");
+  //} else {
+  //  Console.WriteLine($"number a and b are equal since theyre both = {a}");
+  //}
+
+// 4.
+//  Console.Write("Type the first number ");
+//  int number1 = int.Parse(Console.ReadLine()!);
+//  Console.Write("Type the second number ");
+//  int number2 = int.Parse(Console.ReadLine()!);
+//  Console.Write("Type the third number ");
+//  int number3 = int.Parse(Console.ReadLine()!);
+  
+//  int max = number1;
+//  if (number2 > max){
+//    max = number2;
+//  }
+//  if (number3 > max){
+//    max = number3;
+//  }
+
+//  Console.WriteLine($"the largest number is {max}");
+
+// 6.
+//  Console.Write("Type the number ");
+//  int number = int.Parse(Console.ReadLine()!);
+//  if (number%2 == 0) {
+//    Console.WriteLine("this number is even");
+//  } else {
+//    Console.WriteLine("this number is odd");
+//  }
+
+// 8.
+//  Console.Write("Type the number ");
+//  int N = int.Parse(Console.ReadLine()!);
+//  int i = 1;
+//  while (i<=N) {
+//    if (i%2 == 0) {
+//        Console.Write($"{i} ");
+//    }
+//    i++;
+//  }
