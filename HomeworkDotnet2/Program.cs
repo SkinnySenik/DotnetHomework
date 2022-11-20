707f8dbbf8e2bca891fbebe8d8272bcc87b3486a
Arseny <senik006kup@gmail.com>
2022-11-20 14:37:11 +0300

    Dotnet second homework

---
+++ HomeworkDotnet2/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 10.
+
+/* Console.Write("Type the three digit number ");
+string number = Console.ReadLine()!;
+
+if (number.Length == 3) {
+Console.WriteLine($"{number[1]}");
+} else {
+    Console.WriteLine("E R R O R");
+} */
+
+// 13.
+
+/* Console.Write("Type the three digit number ");
+string number = Console.ReadLine()!;
+
+ if (number.Length < 3) {
+   Console.WriteLine("E R R O R. There are only 2 digits on number. Please type a greater number.");
+} else if (number.Length > 5) {
+    Console.WriteLine("I'm afraid i can't tell you the third digit number cause I'm not programmed to count so many numbers");
+    } else { 
+            Console.WriteLine($"{number[2]}"); 
+            } */
+
+// 15.
+
+/* Console.Write("Type the number aligning with day of week ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number > 5 && number < 8) {
+    Console.WriteLine("This number's day of week is part of weekend");
+} else if (number < 1) {
+    Console.WriteLine("This number doesn't have a day of week");
+} else if (number < 6){
+    Console.WriteLine("This number's day of week is part of work week");
+} else {
+    Console.WriteLine("This number doesn't have a day of week");
+} */
+
