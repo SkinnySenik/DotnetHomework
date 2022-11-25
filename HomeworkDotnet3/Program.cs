831352b5f175d0a0b7ee4f2aed41a4adc91911e7
Arseny <senik006kup@gmail.com>
2022-11-25 13:08:01 +0300

    Dotnet third homework

---
+++ HomeworkDotnet3/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 19.
+
+/* Console.Write("Type the 5 digit number ");
+string number = Console.ReadLine()!;
+
+if (number.Length == 5) {
+    if (number[0] == number[4] && number[1] == number[3]) {
+        Console.WriteLine("This number is palindrome");
+    } else {
+        Console.WriteLine("This number is not palindrome");
+    }
+} else {
+    Console.WriteLine("This number is not 5 digit. Please try again");
+}*/
+
+// 21.
+
+/*int[] mas = {3, 6, 8, 2, 1, -7};
+
+double masR = Math.Sqrt(Math.Pow(mas[0] - mas[3], 2) + Math.Pow(mas[1] - mas[4], 2) + Math.Pow(mas[2] - mas[5], 2));
+Console.WriteLine($"{masR:f2}");*/
+
+// 23.
+
+/*Console.Write("Type the number ");
+  int N = int.Parse(Console.ReadLine()!);
+  for (int i = 1; i < N; i++) {
+    Console.Write($"{Math.Pow(i, 3)}, ");
+  } 
+  Console.Write($"{Math.Pow(N, 3)}.");*/
